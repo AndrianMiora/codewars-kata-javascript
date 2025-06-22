2e6285f9839df0dc2a88ac19c4912aad5d560889
Miora Andrian <miora.andri@icloud.com>
2025-06-22 16:29:07 +0300

    Updating totalScore.js

--- totalScore.js
+++ totalScore.js
@@ -5,9 +5,9 @@ function points(games) {
     for (let result of games) {
         const [x, y] = result.split(':').map(Number); 
         if (x > y) {
-            totalPoints += 3; 
+            totalPoints += 3; // situation one of a win
         } else if (x === y) {
-            totalPoints += 1; 
+            totalPoints += 1; // situation two of a draw
         }
     }
 
